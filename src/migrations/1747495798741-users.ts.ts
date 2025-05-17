df9052df38f9e0f3fb0910f8315f626297d49578
JesusZuluaga-M <citozuluaga44@gmail.com>
2025-05-17 15:55:31 -0500

    Format SQL query for user table creation in migration

--- src/migrations/1747495798741-users.ts.ts
+++ src/migrations/1747495798741-users.ts.ts
@@ -2,13 +2,15 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Users_ts1747495798741 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE TABLE users (
-                                    id SERIAL PRIMARY KEY,
-                                    username VARCHAR NOT NULL UNIQUE,
-                                    password VARCHAR NOT NULL,
-                                    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-                                    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-                                    deleted_at TIMESTAMP WITH TIME ZONE);`);
+    await queryRunner.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR NOT NULL UNIQUE,
+        password VARCHAR NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        deleted_at TIMESTAMP WITH TIME ZONE
+      );`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
